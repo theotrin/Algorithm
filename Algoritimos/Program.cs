4f274dec6cb8a2ada626c87a16b382f2ac6f5a56
theotrin <theopinheiro12@gmail.com>
2024-12-17 19:42:25 -0400

    feat: adcionado implementação de outros algoritimos

--- Algoritimos/Program.cs
+++ Algoritimos/Program.cs
@@ -15,16 +15,29 @@
 //}
 
 //Selection Sort
+//{
+//    int[] array = { 64, 25, 12, 22, 11 };
+//    SelectionSort selection = new ();
+//    Console.WriteLine("Array original:");
+//    selection.PrintArray(array);
+
+//    selection.Execute(array);
+
+//    Console.WriteLine("\nArray ordenado:");
+//    selection.PrintArray(array);
+//}
+
+//Insertion Sort
 {
-    int[] array = { 64, 25, 12, 22, 11 };
-    SelectionSort selection = new ();
-    Console.WriteLine("Array original:");
-    selection.PrintArray(array);
+    int[] array = { 7, 3, 5, 2, 4, 1 };
+    InsertionSort insertion = new();
 
-    selection.Execute(array);
+    Console.WriteLine("Array original: ");
+    insertion.PrintArray(array);
 
-    Console.WriteLine("\nArray ordenado:");
-    selection.PrintArray(array);
+    insertion.Execute(array);
+
+    Console.WriteLine("\nArray ordenado: ");
+    insertion.PrintArray(array);
 }
-
 
